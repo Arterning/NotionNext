791ce43d79e0d640ce7be2785588fe0b1f12a3f2
Arterning <ning.huang.ridson@gmail.com>
2023-10-13 15:42:14 +0800

    fix lint err

--- components/ProjectCard/Card.js
+++ components/ProjectCard/Card.js
@@ -9,7 +9,8 @@ const ProjectCard = ({ title, description, imgSrc, href }) => (
       }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
     >
       {imgSrc &&
-        (href ? (
+        (href
+          ? (
           <Link href={href} aria-label={`Link to ${title}`}>
             <Image
               alt={title}
@@ -19,7 +20,8 @@ const ProjectCard = ({ title, description, imgSrc, href }) => (
               height={306}
             />
           </Link>
-        ) : (
+            )
+          : (
           <Image
             alt={title}
             src={imgSrc}
@@ -27,16 +29,18 @@ const ProjectCard = ({ title, description, imgSrc, href }) => (
             width={544}
             height={306}
           />
-        ))}
+            ))}
       <div className="p-6">
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-          {href ? (
+          {href
+            ? (
             <Link href={href} aria-label={`Link to ${title}`}>
               {title}
             </Link>
-          ) : (
-            title
-          )}
+              )
+            : (
+                title
+              )}
         </h2>
         <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
         {href && (
